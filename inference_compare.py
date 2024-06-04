6d75e065646f667793590379e03c9fab1764cb4a
carzh <wolfivyaura@gmail.com>
2024-06-04 20:42:54 +0000

    slightly more efficient decode loop? doesn't decode until the end

--- inference_compare.py
+++ inference_compare.py
@@ -11,10 +11,10 @@ if len(sys.argv) > 0:
     streaming_mode = bool(sys.argv[1])
     print("set streaming mode to: ", streaming_mode)
 
-# prompt = "Why is the sky blue?"
+prompt = "<|user|>\nWhy is the sky blue?<|endoftext|>\n<|assistant|>\n"
 # prompt = "<|user|>\nTell me a NASA joke!<|endoftext|>\n<|assistant|>\n"
 # prompt = "<|user|>\nI am making mayonnaise, it was starting to thicken but now it has become runny and liquid again, is there any way to salvage it?<|endoftext|>\n<|assistant|>\n"
-prompt = "<|user|>\nWhy Aristotelian view of physics (impetus and stuff) is wrong?<|end|>\n<|assistant|>\n"
+# prompt = "<|user|>\nWhy Aristotelian view of physics (impetus and stuff) is wrong?<|end|>\n<|assistant|>\n"
 
 modelpath = "microsoft/Phi-3-mini-4k-instruct"
 max_tokens = 800
@@ -31,29 +31,23 @@ def inference(onnx_model_path, tokenizer, prompt, naive_streaming):
     inference_model = onnxruntime.InferenceSession(onnx_model_path)
 
     next_token_id = -1
-    accumulated_prompt = prompt
-    # put the accumulated prompt through the tokenizer, take just the input ids from the resulting batch encoding, flatten, 
-    # convert to list
-    accumulated_ids = tokenizer(accumulated_prompt, return_tensors = "np")["input_ids"][0].tolist()
+    accumulated_ids = tokenizer(prompt, return_tensors = "np")["input_ids"]
     output_names = ["logits"]
     generated_tokens = 0
 
     if naive_streaming:
-        print(accumulated_prompt)
+        print(prompt)
         print()
 
     while next_token_id != tokenizer.eos_token_id:
         if generated_tokens >= max_tokens:
             break
 
-        input_batch_encoding = hf_tokenizer(accumulated_prompt, return_tensors = "np")
-
-        # adding position ids directly to batch encoding will result in type error
         input_dict = {
-            "input_ids": input_batch_encoding["input_ids"],
-            "attention_mask": input_batch_encoding["attention_mask"],
-            "position_ids": get_position_ids(torch.from_numpy(input_batch_encoding["attention_mask"]))
+            "input_ids": accumulated_ids,
+            "attention_mask": torch.ones(size=accumulated_ids.shape, dtype=torch.int64).numpy(),
         }
+        input_dict["position_ids"] = get_position_ids(torch.from_numpy(input_dict["attention_mask"]))
 
         # run returns a list of numpy values, take the first one -> resulting shape = 
         # [batch_size, sequence_length, vocab_size]
@@ -62,21 +56,20 @@ def inference(onnx_model_path, tokenizer, prompt, naive_streaming):
         last_token_distribution = results_np[0][-1]
 
         next_token_id = last_token_distribution.argmax()
-        accumulated_ids.append(next_token_id)
-
-        accumulated_prompt = hf_tokenizer.batch_decode([accumulated_ids], skip_special_tokens = True)
+        accumulated_ids = np.append(accumulated_ids, [[next_token_id]], axis=1)
 
         generated_tokens += 1
 
         if naive_streaming:
             print(hf_tokenizer.decode(next_token_id), " ", sep = "", end = "", flush = True)
         
+    accumulated_prompt = hf_tokenizer.batch_decode([accumulated_ids], skip_special_tokens = True)
     return accumulated_ids, accumulated_prompt
 
 pre_ft_acc_ids, pre_ft_acc_text = inference("exported_torch_model_no_ft.onnx", hf_tokenizer, prompt, streaming_mode)
 post_ft_acc_ids, post_ft_acc_text = inference("exported_model_ft_2_layers.onnx", hf_tokenizer, prompt, streaming_mode)
 print()
 print()
-print("before finetune: ", pre_ft_acc_text)
+print("before finetune: ", pre_ft_acc_text[0])
 print("=" * 50)
-print("after finetune: ", post_ft_acc_text)
+print("after finetune: ", post_ft_acc_text[0])
